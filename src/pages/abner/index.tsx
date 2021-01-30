13f72167b7c47e1b09dde0cb2323c706ced88fd2
Abner Fonseca <abner.borda@gmail.com>
2021-01-30 02:30:29 -0300

    edit config eslint add routes

---
+++ src/pages/abner/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Abner: React.FC = () => {
+  return (
+    <div>
+      <h1>Abner fonseca</h1>
+    </div>
+  )
+}
+
+export default Abner
---
+++ src/pages/will/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Will: React.FC = () => {
+  return (
+    <div>
+      <h1>Will</h1>
+    </div>
+  )
+}
+
+export default Will
